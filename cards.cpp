b914b362de7b6c0c109aa2085efd5f7c2b2dbd47
dgozos <38340601+dgozos@users.noreply.github.com>
2018-04-17 18:19:40 -0700

    Update cards.cpp

--- cards.cpp
+++ cards.cpp
@@ -1,38 +1,69 @@
 // UOProject.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#include <iostream>
-#include <string>
-using namespace std;
+
+
 /*
-Displays a card visual.
+Displays 2 types of card visuals.
 */
-int main()
+class FaceUp_Card
 {
-	string s1 = "|";
-	string s2 = "              |\n";
-	string s3 = " ______________\n";
-	string s4 = "           |\n";
-	string s5 = "|           ";
-	string s6 = "  |\n";
-	string s7 = "|______________|\n";
+	public:
+		void printCard()
+		{
+			cout << s3;
+			cout << s1 << s2;
+			cout << s1 << "  J" << s4;
+			cout << s1 << s2;
+			cout << s1 << s2;
+			cout << s1 << s2;
+			cout << s1 << s2;
+			cout << s1 << s2;
+			cout << s1 << s2;
+			cout << s5 << "J  " << s6;
+			cout << s7;
+			system("PAUSE");
+		}
 
-	cout << s3;
-	cout << s1 << s2;
-	cout << s1 << "  J" << s4;
-	cout << s1 << s2;
-	cout << s1 << s2;
-	cout << s1 << s2;
-	cout << s1 << s2;
-	cout << s1 << s2;
-	cout << s1 << s2;
-	cout << s5 << "J" << s6;
-	cout << s7;
+	private: 
+		string s1 = "|";
+		string s2 = "              |\n";
+		string s3 = " ______________\n";
+		string s4 = "           |\n";
+		string s5 = "|           ";
+		string s6 = "|\n";
+		string s7 = "|______________|\n";
+		string s8 = "  *  *  *  *  ";
+		string s9 = "    *  *  *   ";
 
+};
 
-	system("PAUSE"); 
+class FaceDown_Card
+{
+	public:
+		void printCard2()
+		{
+			cout << s3;
+			cout << s1 << s2;
+			cout << s1 << s9 << s6;
+			cout << s1 << s8 << s6;
+			cout << s1 << s9 << s6;
+			cout << s1 << s8 << s6;
+			cout << s1 << s9 << s6;
+			cout << s1 << s8 << s6;
+			cout << s1 << s9 << s6;
+			cout << s1 << s8 << s6;
+			cout << s7;
+			system("PAUSE");
+		}
 
-	return 0;
+	private:
+		string s1 = "|";
+		string s2 = "              |\n";
+		string s3 = " ______________\n";
+		string s6 = "|\n";
+		string s7 = "|______________|\n";
+		string s8 = "  *  *  *  *  ";
+		string s9 = "    *  *  *   ";
 
-}
+};
