d5906f7427290e15767f1c55ac7d34e8aec6addf
nolanmccabe <34149709+nolanmccabe@users.noreply.github.com>
2018-04-17 21:21:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,46 @@
 //
 //  Created by Nolan McCabe on 4/12/18.
 //
+#include <iostream>
+#include <stdio.h>     /* printf, scanf, puts, NULL */
+#include <stdlib.h>    /* srand, rand */
+#include <time.h>
+void checkCard (int card, int suit)
+{
+    if()
+}
 
-#include <stdio.h>
+
+int main()
+{
+    srand (time(NULL)); //random seed
+    bool gameLoop = true; //Global variable to loop game
+
+
+    Banner Banner;
+    /*
+    Banner.Intro;
+    Banner.Menu 
+    */
+
+    while (gameLoop)
+    {
+       //Player First Card
+
+       //Computer First Card
+
+       //Player Second Card
+
+       //Computer Second Card
+
+       //Player Choice
+
+       //Computer Choice
+
+       //Win or lose
+    }
+
+
+
+
+}
