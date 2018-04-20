768f4362d387df44ade7f4cdc8130f3c18674250
dgozos <38340601+dgozos@users.noreply.github.com>
2018-04-19 21:25:48 -0700

    Add files via upload

---
+++ JackBanner.cpp
@@ -0,0 +1,43 @@
+// JackBanner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void SIMPLE()
+{
+	cout << "#####" << "  " << "#####" << "  " << "#     #" << "  " << "#####" << "  " << "#    " << "  " << "#####" << endl;
+	cout << "#    " << "  " << "  #  " << "  " << "##  ## " << "  " << "#   #" << "  " << "#    " << "  " << "#    " << endl;
+	cout << "#####" << "  " << "  #  " << "  " << "#  #  #" << "  " << "#####" << "  " << "#    " << "  " << "#####" << endl;
+	cout << "    #" << "  " << "  #  " << "  " << "#     #" << "  " << "#    " << "  " << "#    " << "  " << "#    " << endl;
+	cout << "#####" << "  " << "#####" << "  " << "#     #" << "  " << "#    " << "  " << "#####" << "  " << "#####" << endl;
+}
+
+void JACK()
+{
+	cout << "#####" << "  " << "  #  " << "  " << "#####" << "  " << "#    #" << endl;
+	cout << "  #  " << "  " << " # # " << "  " << "#   #" << "  " << "#  # " << endl;
+	cout << "  #  " << "  " << "#####" << "  " << "#    " << "  " << "##    " << endl;
+	cout << "# #  " << "  " << "#   #" << "  " << "#   #" << "  " << "#  # " << endl;
+	cout << "###  " << "  " << "#   #" << "  " << "#####" << "  " << "#    #" << endl;
+}
+
+int main()
+{
+	
+	SIMPLE();
+	cout << "\n";
+	JACK();
+	cout << "\n";
+	cout << "CREATED BY:\n";
+	cout << "\n";
+	cout << "OVERWORKED & UNDERPAID (Copyright 2018)\n";
+	cout << "\n";
+	cout << "Danna Gozos\n";
+	cout << "Nolan McCabe\n";
+	cout << "Byron Ford\n";
+	cout << "Marco Rosales\n";
+	system("PAUSE");
+}
