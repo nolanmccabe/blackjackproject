8d2b5eced291da89c05b5343bc74bfb6366b50a0
nolanmccabe <34149709+nolanmccabe@users.noreply.github.com>
2018-04-29 16:25:50 -0700

    Create includes.h

---
+++ src/includes.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <stdio.h> //printf, scanf, puts, NULL
+#include <time.h> //set time 
+#include <windows.h> //Sleep function, Windows Only
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdlib.h>
+#include "cards.h" //Outputs a card face to terminal
+#include "values.h" //Applies a value to a face card
+#include "turn.h" //Player's turn
+#include "random.h" //Random Number generator for deck
+#include "banner.h" //banner
