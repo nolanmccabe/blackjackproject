484138085eaa3c5b5863981cb78e2320ed7f608b
nolanmccabe <34149709+nolanmccabe@users.noreply.github.com>
2018-04-29 04:06:36 -0700

    Update turn.cpp

--- src/turn.cpp
+++ src/turn.cpp
@@ -1,24 +1,24 @@
 #include "turn.h"
 
-bool playerDecision(int handValue)
+int decisionNumber(int handValue)
 {
+	enum playerChoice {Hit = 1, Hold, Fold, Error};
 	int playerInput;
-	enum playerDecision{ Hit = 1, Hold, Fold };
 	std::cout << "Hand Total: " << handValue << std::endl;
 	std::cout << "Player, what would you like to do?" << std::endl;
 	std::cout << "HIT [1]" << "  " << "HOLD [2]" << "  " << "FOLD [3]" << std::endl;
 	std::cin >> playerInput; //DOES NOT CHECK FOR WRONG INPUT
 	if (playerInput == Hit)
 	{
-		return true;
+		return Hit;
 	}
 	if (playerInput == Hold)
 	{
-		return false;
+		return Hold;
 	}
 	if (playerInput == Fold)
 	{
-		return false;
+		return Fold;
 	}
-	else return false;
+	else return Error;
 }
