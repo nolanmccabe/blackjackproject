5a3611f588d3e030e60c82794f64f9a836b32ea7
nolanmccabe <34149709+nolanmccabe@users.noreply.github.com>
2018-04-29 02:45:38 -0700

    Update values.cpp

--- src/values.cpp
+++ src/values.cpp
@@ -1,20 +1,90 @@
 #include "values.h"
 
-Values::Values(int value, int suit)
+int assignValue(int card)
 {
-	m_getValue = values_of_card[value];
-	m_getFace = face_of_card(value);
-	m_getSuit = suit_of_card(suit);
-}
-int Values::assignValue() const
-{
-	return m_getValue;
-}
-int Values::assignFace() const
-{
-	return m_getFace;
-}
-int Values::assignSuit() const
-{
-	return m_getSuit;
+	if (card >= 0 && card <= 31)
+	{
+		if (card >= 0 && card <= 15)
+		{
+			if (card >= 0 && card <= 7)
+			{
+				if (card >= 0 && card <= 3)
+				{
+					return 99;
+				}
+				if (card >= 4 && card <= 7)
+				{
+					return 2;
+				}
+			}
+			if (card >= 8 && card <= 15)
+			{
+				if (card >= 8 && card <= 11)
+				{
+					return 3;
+				}
+				if (card >= 12 && card <= 15)
+				{
+					return 4;
+				}
+			}
+		}
+		if (card >= 16 && card <= 31)
+		{
+			if (card >= 16 && card <= 23)
+			{
+				if (card >= 16 && card <= 19)
+				{
+					return 5;
+				}
+				if (card >= 20 && card <= 23)
+				{
+					return 6;
+				}
+
+			}
+			if (card >= 24 && card <= 31)
+			{
+				if (card >= 24 && card <= 27)
+				{
+					return 7;
+				}
+				if (card >= 28 && card <= 31)
+				{
+					return 8;
+				}
+			}
+			
+		}
+	}
+	if (card >= 32 && card <= 51)
+	{
+		if (card >= 32 && card <= 43)
+		{
+			if (card >= 32 && card <= 35)
+			{
+				return 9;
+			}
+			if (card >= 36 && card <= 39)
+			{
+				return 10;
+			}
+			if (card >= 40 && card <= 43)
+			{
+				return 10;
+			}
+		}
+		if (card >= 44 && card <= 51)
+		{
+			if (card >= 44 && card <= 47)
+			{
+				return 10;
+			}
+			if (card >= 48 && card <= 51)
+			{
+				return 10;
+			}
+		}
+	}
+	else return 99;
 }
