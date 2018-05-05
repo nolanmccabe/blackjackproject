e01dc93dc77b21028a55581fe3ce1a2c050a1ca7
nolanmccabe <34149709+nolanmccabe@users.noreply.github.com>
2018-05-05 00:06:56 -0700

    Fixed duplicate cards

--- src/random.cpp
+++ src/random.cpp
@@ -3,6 +3,8 @@
 int randomCard(std::vector<int> &gameDeck)
 {
 	int randomCard = rand() % (gameDeck.size() - 1); //random number generator
+	int dealNewCard = gameDeck[randomCard];
+
 	gameDeck.erase(gameDeck.begin() + randomCard);
-	return randomCard;
+	return dealNewCard;
 };
