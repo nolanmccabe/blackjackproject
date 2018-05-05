c5c65447c0bcb606975b5e833ffe33fc9bbb5472
nolanmccabe <34149709+nolanmccabe@users.noreply.github.com>
2018-05-04 20:59:34 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,10 @@ int main()
 	system("CLS");
 	srand(time(NULL)); //random seed
 	bool gameLoop = true; //Global variable to loop game
-	enum gameIdentifier {playerID, computerID};
-	enum gameCondition {playerHold, playerBust, playerFold, computerHold, computerBust};
+	enum gameIdentifier {playerID, computerID}; //unique ID for players
+	enum gameCondition {playerHold, playerBust, playerFold, computerHold, computerBust}; //The game state of the player
+	int playerHandTotal = 0;
+	int computerHandTotal = 0;
 	int playerEndGameResult, computerEndGameResult;
 
 	while (gameLoop)
@@ -30,42 +32,42 @@ int main()
 		std::vector<int> gameDeck {1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
 			21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,
 			46,47,48,49,50,51,52}; //Game deck
-		int playerHandTotal; //Accumulated hand total
-		bool playerTurn = true; //Player's turn loop, ends when hold or fold
+		//int playerHandTotal; //Accumulated hand total
+		bool playerTurn = true; //Player's turn loop, ends when hold, fold, or bust
 		bool computerTurn = true; //Computer's turn, automated
 
 	/*---------------------------------------------Dealing Cards ----------------------------------------------------*/
 
 		std::cout << "Shuffling the deck..." << std::endl;
-		Sleep(20);
+		Sleep(2000);
 		std::cout << "Dealing cards" << std::endl;
-		Sleep(20);
+		Sleep(2000);
 
 		//Player's First Card
 		playerHandCards.push_back(randomCard(gameDeck));
-		playerHandValues.push_back(assignValue(playerHandCards[0], playerID));
+		playerHandValues.push_back(assignValue(playerHandCards[0], playerID, playerHandTotal));
 
 		//Player's Second Card
 		playerHandCards.push_back(randomCard(gameDeck));
-		playerHandValues.push_back(assignValue(playerHandCards[1], playerID));
+		playerHandValues.push_back(assignValue(playerHandCards[1], playerID, playerHandTotal));
 
 		//Computer's First Card
 		computerHandCards.push_back(randomCard(gameDeck));
-		computerHandValues.push_back(assignValue(computerHandCards[0], computerID));
+		computerHandValues.push_back(assignValue(computerHandCards[0], computerID, computerHandTotal));
 
 		//Computer's SecondCard
 		computerHandCards.push_back(randomCard(gameDeck));
-		computerHandValues.push_back(assignValue(computerHandCards[1], computerID));
+		computerHandValues.push_back(assignValue(computerHandCards[1], computerID, computerHandTotal));
 
 
-		std::cout << "[" << playerHandCards[0] << "] [" << playerHandCards[1] << "]"<< std::endl;
-		std::cout << "[X] [" << computerHandCards[1] << "]" << std::endl;
+		std::cout << "[" << playerHandCards[0] << "]  [" << playerHandCards[1] << "]"<< std::endl;
+		std::cout << "[X]   [" << computerHandCards[1] << "]" << std::endl;
 		Sleep(3);
 	/*---------------------------------------------Player's Turn -----------------------------------------------------*/
 		while (playerTurn)
 		{
 			enum decision {Hit = 1, Hold, Fold, Error};
-			int playerHandTotal = std::accumulate(playerHandValues.begin(), playerHandValues.end(), 0);
+			playerHandTotal = std::accumulate(playerHandValues.begin(), playerHandValues.end(), 0);
 			if (playerHandTotal <= 21)
 			{
 				int decisionNumber = playerDecision(playerHandTotal);
@@ -74,7 +76,7 @@ int main()
 					int newRandomCard = randomCard(gameDeck);
 					playerHandCards.push_back(newRandomCard);
 					int playerCardsInHand = playerHandCards.size() - 1;
-					playerHandValues.push_back(assignValue(playerHandCards[playerCardsInHand], playerID));
+					playerHandValues.push_back(assignValue(playerHandCards[playerCardsInHand], playerID, playerHandTotal));
 					continue;
 				}
 				if (decisionNumber == Hold)
@@ -104,13 +106,13 @@ int main()
 		{
 			if (playerEndGameResult == playerHold)
 			{
-				int computerHandTotal = std::accumulate(computerHandValues.begin(), computerHandValues.end(), 0);
+				computerHandTotal = std::accumulate(computerHandValues.begin(), computerHandValues.end(), 0);
 				if (computerHandTotal <= playerHandTotal)
 				{
 					int newRandomCard = randomCard(gameDeck);
 					computerHandCards.push_back(newRandomCard);
 					int computerCardsInHand = computerHandCards.size() - 1;
-					computerHandValues.push_back(assignValue(computerHandCards[computerCardsInHand], computerID));
+					computerHandValues.push_back(assignValue(computerHandCards[computerCardsInHand], computerID, computerHandTotal));
 					continue;
 				}
 				if (computerHandTotal > playerHandTotal && computerHandTotal <= 21)
@@ -126,7 +128,7 @@ int main()
 			}
 			else
 			{
-				computerEndGameResult == computerHold;
+				computerEndGameResult = computerHold;
 				computerTurn = false;
 			}
 			
@@ -136,22 +138,15 @@ int main()
 		{
 			std::cout << "You win! The computer busts!" << std::endl;
 		}
-		if (playerEndGameResult == playerHold && computerEndGameResult == computerHold)
+		else if (playerEndGameResult == playerHold && computerEndGameResult == computerHold)
 		{
-			if (computerHandTotal > playerHandTotal)
-			{
 				std::cout << "The computer wins!" << std::endl;
-			}
-			else
-			{
-				std::cout << "The computer wins!" << std::endl;
-			}
 		}
-		if (playerEndGameResult == playerBust)
+		else if (playerEndGameResult == playerBust)
 		{
 			std::cout << "Bust! The computer wins!" << std::endl;
 		}
-		if (playerEndGameResult == playerFold)
+		else if (playerEndGameResult == playerFold)
 		{
 			std::cout << "You folded! The computer wins by default!" << std::endl;
 		}
@@ -161,10 +156,8 @@ int main()
 
 
 /* TODO:
-	-Fix player input to allow for errors
 	-Add card face graphic
+	-improved computer gameplay (AI)
 	-Game dialogue
-	-Adjust game ending
-	-Add Banner
 	-Add Gambling
 */
