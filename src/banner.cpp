b4d41a3f9401832919eb6d2046d3514b70d5c545
mccabeno <34149709+nolanmccabe@users.noreply.github.com>
2021-03-14 17:03:57 -0700

    Update banner.cpp

--- src/banner.cpp
+++ src/banner.cpp
@@ -18,6 +18,5 @@ void banner()
 	std::cout << "# #  " << "  " << "#   #" << "  " << "#   #" << "  " << "#  # " << std::endl;
 	std::cout << "###  " << "  " << "#   #" << "  " << "#####" << "  " << "#    #" << std::endl;
 	std::cout << std::endl;
-	std::cout << "Created by Byron Ford, Danna Gozos, Marco Rosales, Nolan McCabe" << std::endl;
-	std::cout << "Copyright © 2018 Overworked and Underpaid. All rights reserved." << std::endl;
+	std::cout << "Created by Nolan McCabe" << std::endl;
 }
